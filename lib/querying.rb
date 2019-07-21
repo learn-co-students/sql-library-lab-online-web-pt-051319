83143556b4b3e64e5a5ffbb47ce6ad559daafb03
Kelley Chaplain <kmchaplain@gmail.com>
2019-07-20 23:05:51 -0500

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,9 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, books.year
+  FROM books
+  INNER JOIN series
+  WHERE books.series_id = series.id
+  ORDER BY books.year ASC;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
@@ -16,7 +20,11 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+  FROM series
+  INNER JOIN characters
+  WHERE characters.species = 'human'
+  GROUP BY series.title;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
---
+++ lib/update.sql
@@ -0,0 +1,3 @@
+UPDATE characters
+SET name = 'Martian'
+WHERE ORDER BY(id) DESC LIMIT 1;
